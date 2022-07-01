e46989b73c77f5efd3a78265c2c055d48cb712f4
Darío Luz <45739605+Darluz@users.noreply.github.com>
2022-07-01 18:39:22 -0300

    part 3.c finish

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const { json } = require("express/lib/response");
@@ -13,6 +13,21 @@ app.use(express.json());
 app.use(morgan("tiny"));
 morgan.token("body", (req) => JSON.stringify(req.body));
 app.use(morgan(":method :url :status :body"));
+
+//Error handler middleware
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if(error.name === 'CastError'){
+    return response.status(400).send({error: 'malformatted id'})
+  }
+
+  next(error);
+}
+
+// this has to be the last loaded middleware
+app.use(errorHandler);
+
 
 const getRandomInt = (max) => Math.floor(Math.random() * max);
 
@@ -63,15 +78,11 @@ app.get("/api/persons/:id", (request, response) => {
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  let person = persons.find((person) => person.id === id);
-
-  if (person) {
-    persons = persons.filter((person) => person.id !== id);
-    response.status(204).end();
-  } else {
-    response.status(404).end();
-  }
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -94,14 +105,29 @@ app.post("/api/persons", (request, response) => {
   const person = new Person({
     name: body.name,
     number: body.number,
-    date: new Date()
+    date: new Date(),
   });
 
-  person.save().then(savedPerson => response.json(savedPerson));
+  person.save().then((savedPerson) => response.json(savedPerson));
+});
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+  .then(updatedNote => {
+    response.json(updatedNote);
+  })
+  .catch(error => next(error))
 })
 
 const PORT = process.env.PORT;
 console.log(PORT);
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 });
